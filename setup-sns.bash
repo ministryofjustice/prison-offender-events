8e8808beb597962283e14bebdb1e95bbe6c55bc8
Peter Phillips <peter@greenthistle.co.uk>
2019-10-22 15:48:48 +0100

    DT-315: Add setup of case note queues including dlq

--- setup-sns.bash
+++ setup-sns.bash
@@ -6,3 +6,12 @@ aws --endpoint-url=http://localhost:4575 sns subscribe \
     --protocol sqs \
     --notification-endpoint http://localhost:4576/queue/event_queue \
     --attributes '{"FilterPolicy":"{\"eventType\":[\"EXTERNAL_MOVEMENT_RECORD-INSERTED\", \"BOOKING_NUMBER-CHANGED\"]}"}'
+
+aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name case_note_dlq
+aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name case_note_queue 
+aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/case_note_queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:case_note_dlq\"}"}'
+aws --endpoint-url=http://localhost:4575 sns subscribe \
+    --topic-arn arn:aws:sns:eu-west-2:000000000000:offender_events \
+    --protocol sqs \
+    --notification-endpoint http://localhost:4576/queue/case_note_queue \
+    --attributes '{"FilterPolicy":"{\"eventType\":[ \"GEN-OSE\", {\"prefix\": \"NEG\"}, {\"prefix\": \"KA\"} ] }"}'
