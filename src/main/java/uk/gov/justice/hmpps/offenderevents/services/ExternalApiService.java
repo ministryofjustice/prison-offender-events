df34a18fa057113800a9f66d2db66287739992d0
Michael Willis <michael.willis@digital.justice.gov.uk>
2020-03-09 11:53:38 +0000

    DT-610 Add timeouts

--- src/main/java/uk/gov/justice/hmpps/offenderevents/services/ExternalApiService.java
+++ src/main/java/uk/gov/justice/hmpps/offenderevents/services/ExternalApiService.java
@@ -1,7 +1,7 @@
 package uk.gov.justice.hmpps.offenderevents.services;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,38 +9,37 @@ import org.springframework.web.util.UriComponentsBuilder;
 import uk.gov.justice.hmpps.offenderevents.config.OffenderEventsProperties;
 import uk.gov.justice.hmpps.offenderevents.model.OffenderEvent;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 @Service
-@AllArgsConstructor
 @Slf4j
 public class ExternalApiService {
 
     private final OffenderEventsProperties properties;
     private final WebClient oauth2WebClient;
+    private final Duration timeout;
+
+    public ExternalApiService(final OffenderEventsProperties properties, final WebClient oauth2WebClient, @Value("${api.event-timeout:30s}") final Duration timeout) {
+        this.properties = properties;
+        this.oauth2WebClient = oauth2WebClient;
+        this.timeout = timeout;
+    }
 
     public List<OffenderEvent> getEvents(final LocalDateTime fromDateTime, final LocalDateTime toDateTime) {
         final var uri = getEventUriBuilder(fromDateTime, toDateTime).build().toUri();
-        try {
-            return oauth2WebClient.get()
-                    .uri(uri)
-                    .attributes(clientRegistrationId("custody-api"))
-                    .retrieve()
-                    .bodyToMono(new ParameterizedTypeReference<List<OffenderEvent>>() {
-                    })
-                    .doOnError(error -> log.error("Error occurred", error))
-                    .block();
-        } catch (Exception e) {
-            return List.of();
-        }
+        return oauth2WebClient.get()
+                .uri(uri)
+                .attributes(clientRegistrationId("custody-api"))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<OffenderEvent>>() {})
+                .block(timeout);
     }
 
     private UriComponentsBuilder getEventUriBuilder(final LocalDateTime fromDateTime, final LocalDateTime toDateTime) {
-        // bit naff, but the template handler holds the root uri that we need, so have to create a uri from it to then pass to the builder
-
         return UriComponentsBuilder.fromUriString(properties.getCustodyApiBaseUrl() + "/api/events")
                 .queryParam("sortBy", "TIMESTAMP_ASC")
                 .queryParam("from", fromDateTime)
