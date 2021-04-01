13d70622d7bfaa2152512f6004c2a38b6f444b14
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-04-01 10:27:47 +0100

    DT-1829 Upgrades (#66)
    * DT-1829 useLatestVersions
    
    * DT-1829 upgrade kotlin spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
-  kotlin("plugin.spring") version "1.4.30"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.4"
+  kotlin("plugin.spring") version "1.4.32"
 }
 
 configurations {
@@ -12,7 +12,7 @@ dependencies {
   annotationProcessor("org.projectlombok:lombok:1.18.18")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.5.3")
+  runtimeOnly("org.flywaydb:flyway-core:7.7.1")
   runtimeOnly("org.postgresql:postgresql:42.2.19")
 
   compileOnly("org.projectlombok:lombok:1.18.18")
@@ -27,8 +27,8 @@ dependencies {
 
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
-  implementation("net.javacrumbs.shedlock:shedlock-spring:4.20.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.20.0")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:4.22.1")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.22.1")
 
   implementation("javax.activation:activation:1.1.1")
   implementation("javax.transaction:javax.transaction-api:1.3")
@@ -37,17 +37,17 @@ dependencies {
 
   implementation("net.sf.ehcache:ehcache:2.10.6")
   implementation("org.apache.commons:commons-text:1.9")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.1")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.2")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
   implementation("com.google.code.gson:gson:2.8.6")
-  implementation("com.google.guava:guava:30.1-jre")
+  implementation("com.google.guava:guava:30.1.1-jre")
 
   testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   testAnnotationProcessor("org.projectlombok:lombok:1.18.18")
   testCompileOnly("org.projectlombok:lombok:1.18.18")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.24.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("junit:junit:4.13.2")
