a63e9483148b45289e6b97275238228a71a87117
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-06-09 15:51:34 +0100

    DT-2054 service to call community-api recall (not used yet)

--- src/test/java/uk/gov/justice/hmpps/offenderevents/services/wiremock/CommunityApiMockServer.java
+++ src/test/java/uk/gov/justice/hmpps/offenderevents/services/wiremock/CommunityApiMockServer.java
@@ -9,4 +9,228 @@ public class CommunityApiMockServer extends WireMockServer {
     CommunityApiMockServer() {
         super(8087);
     }
+
+    public void stubForRecall(String nomsNumber) {
+        stubFor(
+            get(String.format("/secure/offenders/nomsNumber/%s/convictions/active/nsis/recall", nomsNumber)).willReturn(
+                aResponse()
+                    .withHeader("Content-Type", "application/json")
+                    .withBody(recall())
+                    .withStatus(200)
+            )
+        );
+    }
+
+    private String recall() {
+        return """
+            {
+                "nsis": [
+                    {
+                        "nsiId": 2500032231,
+                        "nsiType": {
+                            "code": "REC",
+                            "description": "Request for Recall"
+                        },
+                        "nsiSubType": {
+                            "code": "REC02",
+                            "description": "Emergency"
+                        },
+                        "nsiStatus": {
+                            "code": "REC01",
+                            "description": "Recall Initiated"
+                        },
+                        "statusDateTime": "2021-05-13T12:42:00",
+                        "referralDate": "2021-05-12",
+                        "lengthUnit": "Months",
+                        "nsiManagers": [
+                            {
+                                "probationArea": {
+                                    "probationAreaId": 1500001006,
+                                    "code": "N07",
+                                    "description": "NPS London",
+                                    "organisation": {
+                                        "code": "NPS",
+                                        "description": "National Probation Service"
+                                    }
+                                },
+                                "team": {
+                                    "code": "N07UAT",
+                                    "description": "Unallocated Team(N07)",
+                                    "localDeliveryUnit": {
+                                        "code": "N07NPSA",
+                                        "description": "N07 Division"
+                                    },
+                                    "teamType": {
+                                        "code": "N07NPS1",
+                                        "description": "N07 LDU 1"
+                                    },
+                                    "district": {
+                                        "code": "N07NPSA",
+                                        "description": "N07 Division"
+                                    },
+                                    "borough": {
+                                        "code": "N07100",
+                                        "description": "N07 Cluster 1"
+                                    }
+                                },
+                                "staff": {
+                                    "staffCode": "N07UATU",
+                                    "staffIdentifier": 1500543296,
+                                    "staff": {
+                                        "forenames": "Unallocated Staff(N07)",
+                                        "surname": "Staff"
+                                    },
+                                    "teams": [
+                                        {
+                                            "code": "N07UAT",
+                                            "description": "Unallocated Team(N07)",
+                                            "localDeliveryUnit": {
+                                                "code": "N07NPSA",
+                                                "description": "N07 Division"
+                                            },
+                                            "teamType": {
+                                                "code": "N07NPS1",
+                                                "description": "N07 LDU 1"
+                                            },
+                                            "district": {
+                                                "code": "N07NPSA",
+                                                "description": "N07 Division"
+                                            },
+                                            "borough": {
+                                                "code": "N07100",
+                                                "description": "N07 Cluster 1"
+                                            }
+                                        }
+                                    ]
+                                },
+                                "startDate": "2021-05-12"
+                            }
+                        ],
+                        "intendedProvider": {
+                            "probationAreaId": 1500001006,
+                            "code": "N07",
+                            "description": "NPS London",
+                            "organisation": {
+                                "code": "NPS",
+                                "description": "National Probation Service"
+                            }
+                        },
+                        "active": true
+                    },
+                    {
+                        "nsiId": 2500032227,
+                        "nsiType": {
+                            "code": "REC",
+                            "description": "Request for Recall"
+                        },
+                        "nsiSubType": {
+                            "code": "REC02",
+                            "description": "Emergency"
+                        },
+                        "nsiOutcome": {
+                            "code": "REC03",
+                            "description": "Recall Rejected by NPS"
+                        },
+                        "nsiStatus": {
+                            "code": "REC08",
+                            "description": "Recall Submitted to PPCS"
+                        },
+                        "statusDateTime": "2021-05-13T12:22:00",
+                        "referralDate": "2021-05-13",
+                        "lengthUnit": "Months",
+                        "nsiManagers": [
+                            {
+                                "probationArea": {
+                                    "probationAreaId": 1500001006,
+                                    "code": "N07",
+                                    "description": "NPS London",
+                                    "organisation": {
+                                        "code": "NPS",
+                                        "description": "National Probation Service"
+                                    }
+                                },
+                                "team": {
+                                    "code": "N07UAT",
+                                    "description": "Unallocated Team(N07)",
+                                    "localDeliveryUnit": {
+                                        "code": "N07NPSA",
+                                        "description": "N07 Division"
+                                    },
+                                    "teamType": {
+                                        "code": "N07NPS1",
+                                        "description": "N07 LDU 1"
+                                    },
+                                    "district": {
+                                        "code": "N07NPSA",
+                                        "description": "N07 Division"
+                                    },
+                                    "borough": {
+                                        "code": "N07100",
+                                        "description": "N07 Cluster 1"
+                                    }
+                                },
+                                "staff": {
+                                    "staffCode": "N07UATU",
+                                    "staffIdentifier": 1500543296,
+                                    "staff": {
+                                        "forenames": "Unallocated Staff(N07)",
+                                        "surname": "Staff"
+                                    },
+                                    "teams": [
+                                        {
+                                            "code": "N07UAT",
+                                            "description": "Unallocated Team(N07)",
+                                            "localDeliveryUnit": {
+                                                "code": "N07NPSA",
+                                                "description": "N07 Division"
+                                            },
+                                            "teamType": {
+                                                "code": "N07NPS1",
+                                                "description": "N07 LDU 1"
+                                            },
+                                            "district": {
+                                                "code": "N07NPSA",
+                                                "description": "N07 Division"
+                                            },
+                                            "borough": {
+                                                "code": "N07100",
+                                                "description": "N07 Cluster 1"
+                                            }
+                                        }
+                                    ]
+                                },
+                                "startDate": "2021-05-13"
+                            }
+                        ],
+                        "intendedProvider": {
+                            "probationAreaId": 1500001006,
+                            "code": "N07",
+                            "description": "NPS London",
+                            "organisation": {
+                                "code": "NPS",
+                                "description": "National Probation Service"
+                            }
+                        },
+                        "active": false
+                    }
+                ]
+            }
+                        """;
+    }
+
+    public void stubForRecallNotFound(String nomsNumber) {
+        stubFor(
+            get(String.format("/secure/offenders/nomsNumber/%s/convictions/active/nsis/recall", nomsNumber)).willReturn(
+                aResponse()
+                    .withHeader("Content-Type", "application/json")
+                    .withBody("""
+                        {
+                           "status": 404
+                        }
+                        """)
+                    .withStatus(404)
+            )
+        );
+
+    }
 }
