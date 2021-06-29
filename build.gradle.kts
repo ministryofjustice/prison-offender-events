a76c6e9736606bb54b133bd6775c4b7d812292b7
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-06-29 11:51:29 +0100

    DT-2259 Use latest versions (and stop pinning ehcache)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.1"
   kotlin("plugin.spring") version "1.5.10"
 }
 
@@ -12,8 +12,8 @@ dependencies {
   annotationProcessor("org.projectlombok:lombok:1.18.20")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.9.0")
-  runtimeOnly("org.postgresql:postgresql:42.2.20")
+  runtimeOnly("org.flywaydb:flyway-core:7.10.0")
+  runtimeOnly("org.postgresql:postgresql:42.2.22")
 
   compileOnly("org.projectlombok:lombok:1.18.20")
 
@@ -29,19 +29,19 @@ dependencies {
   implementation("org.springframework:spring-jms")
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
 
-  implementation("net.javacrumbs.shedlock:shedlock-spring:4.22.1")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.22.1")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:4.24.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.24.0")
 
   implementation("javax.activation:activation:1.1.1")
   implementation("javax.transaction:javax.transaction-api:1.3")
 
   implementation("io.jsonwebtoken:jjwt:0.9.1")
 
-  implementation("net.sf.ehcache:ehcache:2.10.6")
+  implementation("net.sf.ehcache:ehcache")
   implementation("org.apache.commons:commons-text:1.9")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.3")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
-  implementation("com.google.code.gson:gson:2.8.6")
+  implementation("com.google.code.gson:gson:2.8.7")
   implementation("com.google.guava:guava:30.1.1-jre")
 
   // needed for record serialisation
@@ -49,20 +49,20 @@ dependencies {
   implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
   implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.3")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.9")
 
   testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   testAnnotationProcessor("org.projectlombok:lombok:1.18.20")
   testCompileOnly("org.projectlombok:lombok:1.18.20")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.27.0")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("junit:junit:4.13.2")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.27.0")
   testImplementation("org.awaitility:awaitility:4.1.0")
 }
 
