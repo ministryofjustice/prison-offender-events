c4999b92484c71aac97516da8585a7276bab395f
karenmillermoj <karen.miller@digital.justice.gov.uk>
2021-07-06 13:48:06 +0100

    DT-2298 Add Topic Health check

---
+++ src/test/kotlin/uk/gov/justice/hmpps/offenderevents/health/HmppsTopicHealthTest.kt
@@ -0,0 +1,40 @@
+package uk.gov.justice.hmpps.offenderevents.health
+
+import com.amazonaws.services.sns.AmazonSNS
+import com.amazonaws.services.sns.model.GetTopicAttributesResult
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.springframework.boot.actuate.health.Status
+import uk.gov.justice.hmpps.offenderevents.config.SqsConfigProperties
+import java.lang.RuntimeException
+
+internal class HmppsTopicHealthTest {
+  private val awsHMPPSEventsSnsClient = Mockito.mock(AmazonSNS::class.java)
+  private val topicHealth = HmppsTopicHealth(
+    awsHMPPSEventsSnsClient,
+    SqsConfigProperties(
+      "", "", topics = mapOf("hmppsEventTopic" to SqsConfigProperties.TopicConfig(topicArn = "SOME_ARN")),
+      queues = mapOf()
+    )
+  )
+
+  @Test
+  @DisplayName("Topic health is up")
+  fun topicHealthIsUp() {
+    Mockito.`when`(awsHMPPSEventsSnsClient.getTopicAttributes("SOME_ARN")).thenReturn(GetTopicAttributesResult())
+    val health = topicHealth.health()
+    Assertions.assertThat(health.status).isEqualTo(Status.UP)
+    Assertions.assertThat(health.details).containsEntry("hmppsArn", "SOME_ARN")
+  }
+
+  @Test
+  @DisplayName("Topic health is down")
+  fun topicHealthIsDown() {
+    Mockito.`when`(awsHMPPSEventsSnsClient.getTopicAttributes("SOME_ARN")).thenThrow(RuntimeException())
+    val health = topicHealth.health()
+    Assertions.assertThat(health.status).isEqualTo(Status.DOWN)
+    Assertions.assertThat(health.details).containsEntry("hmppsArn", "SOME_ARN")
+  }
+}
