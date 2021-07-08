a89136f404cfbb2f95f9df878d395d298dcec0f2
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-07-08 09:23:14 +0100

    DT-2285 Purge queue before starting tests

--- src/test/kotlin/uk/gov/justice/hmpps/offenderevents/e2e/HouseKeepingIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/hmpps/offenderevents/e2e/HouseKeepingIntegrationTest.kt
@@ -3,11 +3,16 @@ package uk.gov.justice.hmpps.offenderevents.e2e
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.matches
 import org.awaitility.kotlin.untilCallTo
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import uk.gov.justice.hmpps.offenderevents.resource.QueueListenerIntegrationTest
 
 class HouseKeepingIntegrationTest : QueueListenerIntegrationTest() {
+  @BeforeEach
+  internal fun setUp() {
+    purgeQueues()
+  }
 
   @Test
   fun `housekeeping will consume a booking changed message on the dlq and return to main queue`() {
