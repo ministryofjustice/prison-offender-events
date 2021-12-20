5558902ccafdb1fddf4e868b2744f3a772eb1faf
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2021-12-20 14:23:02 +0000

    DT-2956: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.0 includes springboot 2.6.1 and kotlin 1.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.16"
-  kotlin("plugin.spring") version "1.5.31"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
+  kotlin("plugin.spring") version "1.6.0"
   id("org.unbroken-dome.test-sets") version "4.0.0"
 }
 
@@ -14,13 +14,13 @@ testSets {
 
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-  annotationProcessor("org.projectlombok:lombok:1.18.20")
+  annotationProcessor("org.projectlombok:lombok:1.18.22")
 
-  runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.15.0")
-  runtimeOnly("org.postgresql:postgresql:42.2.23")
+  runtimeOnly("com.h2database:h2:2.0.202")
+  runtimeOnly("org.flywaydb:flyway-core:8.2.3")
+  runtimeOnly("org.postgresql:postgresql:42.3.1")
 
-  compileOnly("org.projectlombok:lombok:1.18.20")
+  compileOnly("org.projectlombok:lombok:1.18.22")
 
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-cache")
@@ -33,10 +33,10 @@ dependencies {
 
   implementation("com.amazonaws:aws-java-sdk-sns")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.3")
 
-  implementation("net.javacrumbs.shedlock:shedlock-spring:4.26.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.26.0")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:4.30.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.30.0")
 
   implementation("javax.activation:activation:1.1.1")
   implementation("javax.transaction:javax.transaction-api:1.3")
@@ -45,23 +45,23 @@ dependencies {
 
   implementation("net.sf.ehcache:ehcache")
   implementation("org.apache.commons:commons-text:1.9")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.5")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
-  implementation("com.google.code.gson:gson:2.8.8")
-  implementation("com.google.guava:guava:30.1.1-jre")
+  implementation("com.google.code.gson:gson:2.8.9")
+  implementation("com.google.guava:guava:31.0.1-jre")
 
   // needed for record serialisation
-  implementation("com.fasterxml.jackson.core:jackson-core:2.12.5")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.12.5")
-  implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.5")
+  implementation("com.fasterxml.jackson.core:jackson-core:2.13.1")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
+  implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.1")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.10")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.2")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.2")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.2")
 
   testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-  testAnnotationProcessor("org.projectlombok:lombok:1.18.20")
-  testCompileOnly("org.projectlombok:lombok:1.18.20")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.22")
+  testCompileOnly("org.projectlombok:lombok:1.18.22")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
@@ -69,11 +69,10 @@ dependencies {
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("junit:junit:4.13.2")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
-  testImplementation("org.awaitility:awaitility:4.1.0")
-  testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
+  testImplementation("org.awaitility:awaitility:4.1.1")
+  testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.5.2")
-  testImplementation("org.mockito:mockito-inline:3.12.4")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+  testImplementation("org.mockito:mockito-inline:4.2.0")
 }
 
 java {
