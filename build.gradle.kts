161e537a629cd23256003af67e2a3642f133da9d
petergphillips <peter@greenthistle.co.uk>
2022-03-18 10:30:33 +0000

    SDI-123: ⬆️  Fix owasp builds by bumping versions (#186)
    * SDI-123: ⬆️  Fix owasp builds by bumping versions
    
    * SDI-123: ⬆️  Also set cache key to prevent owasp builds failing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.5-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.0"
   kotlin("plugin.spring") version "1.6.10"
   id("org.unbroken-dome.test-sets") version "4.0.0"
 }
@@ -17,7 +17,7 @@ dependencies {
   annotationProcessor("org.projectlombok:lombok:1.18.22")
 
   runtimeOnly("com.h2database:h2:2.1.210")
-  runtimeOnly("org.flywaydb:flyway-core:8.5.1")
+  runtimeOnly("org.flywaydb:flyway-core:8.5.4")
   runtimeOnly("org.postgresql:postgresql:42.3.3")
 
   compileOnly("org.projectlombok:lombok:1.18.22")
@@ -33,7 +33,7 @@ dependencies {
 
   implementation("com.amazonaws:aws-java-sdk-sns")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.6")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.1")
 
   implementation("net.javacrumbs.shedlock:shedlock-spring:4.33.0")
   implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.33.0")
@@ -45,15 +45,15 @@ dependencies {
 
   implementation("net.sf.ehcache:ehcache")
   implementation("org.apache.commons:commons-text:1.9")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
   implementation("com.google.code.gson:gson:2.9.0")
   implementation("com.google.guava:guava:31.1-jre")
 
   // needed for record serialisation
-  implementation("com.fasterxml.jackson.core:jackson-core:2.13.1")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
-  implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.1")
+  implementation("com.fasterxml.jackson.core:jackson-core:2.13.2")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2")
+  implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.2")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
@@ -70,11 +70,11 @@ dependencies {
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("junit:junit:4.13.2")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.32.0")
-  testImplementation("org.awaitility:awaitility:4.1.1")
-  testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
+  testImplementation("org.awaitility:awaitility:4.2.0")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.6.0")
-  testImplementation("org.mockito:mockito-inline:4.3.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.0.30")
+  testImplementation("org.mockito:mockito-inline:4.4.0")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.0.31")
 }
 
 java {
