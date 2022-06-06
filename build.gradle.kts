b3dfe765715791408f53bc0caa832da0e551ea9f
petergphillips <peter@greenthistle.co.uk>
2022-06-06 11:58:25 +0100

    SDI-218: ⬆️a Use latest plugin with suppressions (#201)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.0-beta-4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.1"
   kotlin("plugin.spring") version "1.6.21"
   id("org.unbroken-dome.test-sets") version "4.0.0"
 }
@@ -17,7 +17,7 @@ dependencies {
   annotationProcessor("org.projectlombok:lombok:1.18.24")
 
   runtimeOnly("com.h2database:h2:2.1.212")
-  runtimeOnly("org.flywaydb:flyway-core:8.5.11")
+  runtimeOnly("org.flywaydb:flyway-core:8.5.12")
   runtimeOnly("org.postgresql:postgresql:42.3.6")
 
   compileOnly("org.projectlombok:lombok:1.18.24")
@@ -35,8 +35,8 @@ dependencies {
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.3")
 
-  implementation("net.javacrumbs.shedlock:shedlock-spring:4.35.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.35.0")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:4.36.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.36.0")
 
   implementation("javax.activation:activation:1.1.1")
   implementation("javax.transaction:javax.transaction-api:1.3")
@@ -55,10 +55,10 @@ dependencies {
   implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
   implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-security:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.9")
 
   testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
@@ -72,8 +72,8 @@ dependencies {
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.35.0")
   testImplementation("org.awaitility:awaitility:4.2.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.6.1")
-  testImplementation("org.mockito:mockito-inline:4.5.1")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.6.2")
+  testImplementation("org.mockito:mockito-inline:4.6.1")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.33")
 }
 
