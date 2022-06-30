3573b3fcc3841294278f4cc9cea6de74f16dd425
Peter Phillips <peter@greenthistle.co.uk>
2022-06-30 13:08:36 +0100

    SDI-257: 🔒️ Suppress false positive for tomcat

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.0-beta-1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.1"
   kotlin("plugin.spring") version "1.7.0"
   id("org.unbroken-dome.test-sets") version "4.0.0"
 }
