2246fbaaed988d73d3b9e9d8301334355f241dcf
petergphillips <peter@greenthistle.co.uk>
2023-10-31 11:19:22 +0000

    SDIT-1129: 🐛 Add response timeouts (#336)

--- src/main/kotlin/uk/gov/justice/hmpps/offenderevents/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offenderevents/config/WebClientConfiguration.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.hmpps.offenderevents.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
@@ -10,42 +12,59 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import java.time.Duration
+import kotlin.apply as kotlinApply
 
 @Configuration
-class WebClientConfiguration(private val properties: OffenderEventsProperties) {
-  @Bean
-  fun prisonApiWebClient(
-    builder: WebClient.Builder,
-    authorizedClientManager: OAuth2AuthorizedClientManager,
-  ): WebClient {
-    val oauth2Client = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager).also {
-      it.setDefaultClientRegistrationId("prison-api")
-    }
-    return builder
-      .baseUrl(properties.prisonApiBaseUrl)
-      .apply(oauth2Client.oauth2Configuration())
-      .exchangeStrategies(
-        ExchangeStrategies.builder().codecs {
-          it.defaultCodecs().maxInMemorySize(-1)
-        }
-          .build(),
-      )
-      .build()
-  }
+class WebClientConfiguration(
+  @Value("\${oauth.api.base.url}") val hmppsAuthBaseUrl: String,
+  @Value("\${prison.api.base.url}") val prisonApiBaseUrl: String,
+  @Value("\${api.health-timeout:2s}") val healthTimeout: Duration,
+  @Value("\${api.timeout:20s}") val timeout: Duration,
+) {
 
-  /*** default web client for health checks */
   @Bean
-  fun healthCheckWebClient(builder: WebClient.Builder): WebClient = builder.build()
+  fun prisonApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient =
+    builder.authorisedWebClient(authorizedClientManager, registrationId = "prison-api", url = prisonApiBaseUrl, timeout)
+
+  @Bean
+  fun hmppsAuthHealthWebClient(builder: WebClient.Builder): WebClient =
+    builder.healthWebClient(hmppsAuthBaseUrl, healthTimeout)
+
+  @Bean
+  fun prisonApiHealthWebClient(builder: WebClient.Builder): WebClient =
+    builder.healthWebClient(prisonApiBaseUrl, healthTimeout)
 
   @Bean
   fun authorizedClientManager(
     clientRegistrationRepository: ClientRegistrationRepository,
     oAuth2AuthorizedClientService: OAuth2AuthorizedClientService,
-  ): OAuth2AuthorizedClientManager {
-    val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
-    return AuthorizedClientServiceOAuth2AuthorizedClientManager(
-      clientRegistrationRepository,
-      oAuth2AuthorizedClientService,
-    ).also { it.setAuthorizedClientProvider(authorizedClientProvider) }
+  ): OAuth2AuthorizedClientManager = AuthorizedClientServiceOAuth2AuthorizedClientManager(
+    clientRegistrationRepository,
+    oAuth2AuthorizedClientService,
+  ).kotlinApply {
+    setAuthorizedClientProvider(OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build())
   }
 }
+
+fun WebClient.Builder.authorisedWebClient(
+  authorizedClientManager: OAuth2AuthorizedClientManager,
+  registrationId: String,
+  url: String,
+  timeout: Duration,
+): WebClient =
+  baseUrl(url)
+    .clientConnector(ReactorClientHttpConnector(HttpClient.create().responseTimeout(timeout)))
+    .exchangeStrategies(ExchangeStrategies.builder().codecs { it.defaultCodecs().maxInMemorySize(-1) }.build())
+    .filter(
+      ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager).kotlinApply {
+        setDefaultClientRegistrationId(registrationId)
+      },
+    )
+    .build()
+
+fun WebClient.Builder.healthWebClient(url: String, healthTimeout: Duration): WebClient =
+  baseUrl(url)
+    .clientConnector(ReactorClientHttpConnector(HttpClient.create().responseTimeout(healthTimeout)))
+    .build()
--- src/main/kotlin/uk/gov/justice/hmpps/offenderevents/health/HealthInfo.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offenderevents/health/HealthInfo.kt
@@ -1,8 +1,5 @@
 package uk.gov.justice.hmpps.offenderevents.health
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.boot.info.BuildProperties
@@ -12,17 +9,8 @@ import org.springframework.stereotype.Component
  * Adds version data to the /health endpoint. This is called by the UI to display API details
  */
 @Component
-class HealthInfo(@param:Autowired(required = false) private val buildProperties: BuildProperties?) : HealthIndicator {
-  companion object {
-    val log: Logger = LoggerFactory.getLogger(this::class.java)
-  }
+class HealthInfo(buildProperties: BuildProperties) : HealthIndicator {
+  private val version: String = buildProperties.version
 
-  private val version: String
-    get() = if (buildProperties == null) "version not available" else buildProperties.version
-
-  override fun health(): Health {
-    val status = Health.up().withDetail("version", version).build()
-    log.info(status.toString())
-    return status
-  }
+  override fun health(): Health = Health.up().withDetail("version", version).build()
 }
