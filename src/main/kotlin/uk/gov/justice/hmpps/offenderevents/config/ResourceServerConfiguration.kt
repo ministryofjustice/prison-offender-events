6ec03dd9594f2aecd23b369186a290fd0efa794b
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-02-12 11:01:24 +0000

    SDIT-1335 Impement hmpps-kotlin-spring-boot-starter lib (#351)

--- src/main/kotlin/uk/gov/justice/hmpps/offenderevents/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offenderevents/config/ResourceServerConfiguration.kt
@@ -1,46 +1,15 @@
 package uk.gov.justice.hmpps.offenderevents.config
 
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.cache.CacheManager
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
-import org.springframework.security.web.SecurityFilterChain
+import uk.gov.justice.hmpps.kotlin.auth.dsl.ResourceServerConfigurationCustomizer
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
-@EnableCaching
 class ResourceServerConfiguration {
   @Bean
-  fun filterChain(http: HttpSecurity): SecurityFilterChain = http {
-    headers { frameOptions { sameOrigin = true } }
-    sessionManagement { sessionCreationPolicy = SessionCreationPolicy.STATELESS }
-    // Can't have CSRF protection as requires session
-    csrf { disable() }
-    authorizeHttpRequests {
-      listOf(
-        "/webjars/**", "/favicon.ico", "/csrf",
-        "/health/**", "/info", "/ping",
-        "/prometheus/**",
-        "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
-        "/queue-admin/retry-all-dlqs",
-      ).forEach { authorize(it, permitAll) }
-      authorize(anyRequest, authenticated)
+  fun resourceServerCustomizer() = ResourceServerConfigurationCustomizer.build {
+    unauthorizedRequestPaths {
+      addPaths = setOf("/queue-admin/retry-all-dlqs")
     }
-    oauth2ResourceServer { jwt { jwtAuthenticationConverter = AuthAwareTokenConverter() } }
-  }.let { http.build() }
-
-  @Bean
-  fun locallyCachedJwtDecoder(
-    @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") jwkSetUri: String,
-    cacheManager: CacheManager,
-  ): JwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).cache(cacheManager.getCache("jwks")).build()
+  }
 }
