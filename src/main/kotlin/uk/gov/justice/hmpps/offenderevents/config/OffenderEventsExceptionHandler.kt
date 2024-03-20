7763a46cc36d81cb3bb87baec92a875632778286
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-03-20 16:40:20 +0000

    SDIT-1601: ✨ Add no resource found handler

--- src/main/kotlin/uk/gov/justice/hmpps/offenderevents/config/OffenderEventsExceptionHandler.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offenderevents/config/OffenderEventsExceptionHandler.kt
@@ -2,17 +2,18 @@ package uk.gov.justice.hmpps.offenderevents.config
 
 import jakarta.validation.ValidationException
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.resource.NoResourceFoundException
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 
 @RestControllerAdvice
-class PrisonerOffenderEventsExceptionHandler {
+class OffenderEventsExceptionHandler {
   @ExceptionHandler(ValidationException::class)
   fun handleValidationException(e: Exception): ResponseEntity<ErrorResponse> {
-    log.info("Validation exception: {}", e.message)
     return ResponseEntity
       .status(BAD_REQUEST)
       .body(
@@ -21,27 +22,21 @@ class PrisonerOffenderEventsExceptionHandler {
           userMessage = "Validation failure: ${e.message}",
           developerMessage = e.message,
         ),
-      )
+      ).also { log.info("Validation exception: {}", e.message) }
   }
+
+  @ExceptionHandler(NoResourceFoundException::class)
+  fun handleNoResourceFoundException(e: NoResourceFoundException): ResponseEntity<ErrorResponse> = ResponseEntity
+    .status(NOT_FOUND)
+    .body(
+      ErrorResponse(
+        status = NOT_FOUND,
+        userMessage = "No resource found failure: ${e.message}",
+        developerMessage = e.message,
+      ),
+    ).also { log.info("No resource found exception: {}", e.message) }
 
   companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
   }
-}
-
-data class ErrorResponse(
-  val status: Int,
-  val errorCode: Int? = null,
-  val userMessage: String? = null,
-  val developerMessage: String? = null,
-  val moreInfo: String? = null,
-) {
-  constructor(
-    status: HttpStatus,
-    errorCode: Int? = null,
-    userMessage: String? = null,
-    developerMessage: String? = null,
-    moreInfo: String? = null,
-  ) :
-    this(status.value(), errorCode, userMessage, developerMessage, moreInfo)
 }
