29a56d6c6efae776657d72fa804332c5c7cbf5cc
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-02-11 11:11:26 +0000

    SDIT-2561: ⬆️  Upgrade hmpps spring boot plugin to 7.7.1 (#451)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.1"
   kotlin("plugin.spring") version "2.1.10"
 }
 
